0584e46db6025c44a77d07a388248fd5c3fbc6cd
alfonsopapa03-spec <alfonsopapa03@gmail.com>
2026-09-01 12:15:29 -0500

    Update ola.py

--- ola.py
+++ ola.py
@@ -24,7 +24,7 @@ from openpyxl.styles import Font, PatternFill, Alignment, Border, Side
 from openpyxl.utils import get_column_letter
 
 # ==================== CONFIGURACIÓN ====================
-SUPABASE_DB_URL = "postgresql://postgres.ntnpckmbyfmjhfskfwyu:Conejito800$@aws-1-us-east-1.pooler.supabase.com:6543/postgreshttps://github.com/alfonsopapa03-spec/alfonsopapaga/edit/main/ola.py"
+SUPABASE_DB_URL = "postgresql://postgres.wiomyjrmsrhcgvhgkbqe:Conejito800$@aws-1-us-west-2.pooler.supabase.com:6543/postgres"
 DIAS_VACACIONES_ANUALES = 15
 
 # ==================== FORMATO COLOMBIANO ====================
